0caa39a70d90a54077c122b4dc70fd2d280faf0c
Andrew Ripka <aripka@pivotal.io>
2017-04-17 22:45:37 -0500

    update to allow submitting a URL as part of script

--- load.sh
+++ load.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 COUNTER=0
 TOP=$1
+
+if [ ! "$2"];then
+	URL=http://localhost:9090
+else
+	URL=$2
+fi
+
+echo $URL
+
 while [ $COUNTER -lt $TOP ]; do
-	curl http://localhost:9090/random
+	curl $URL/random
 	let COUNTER=COUNTER+1
-	sleep .45
+	sleep .25
 done
+
